590f35d61b7518495d06a31049da6e82d2129d72
phasetransition <phasetransition@gmail.com>
2013-10-06 13:53:13 +0900

    Subway

---
+++ SubwayLogin/content.js
@@ -0,0 +1,12 @@
+if (location.href.match(/https:\/\/rapico.jp/)) {
+	document.getElementsByName('card_id')[0].value = '8514511110957912';
+	document.getElementsByName('pin_id')[0].value = '737135';
+}
+
+if (location.href.match(/https:\/\/www.repica.bz/)) {
+	document.getElementsByName('cno1')[0].value = '8514';
+	document.getElementsByName('cno2')[0].value = '5111';
+	document.getElementsByName('cno3')[0].value = '1095';
+	document.getElementsByName('cno4')[0].value = '7912';
+	document.getElementsByName('pin')[0].value = '737135';
+}
