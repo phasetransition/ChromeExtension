e73c386f193ed7aefc6f0387ea5e2ba487fec259
phasetransition <phasetransition@gmail.com>
2014-03-07 23:15:28 +0900

    .

--- AddPrivateLinkToGoogleMenu/content.js
+++ AddPrivateLinkToGoogleMenu/content.js
@@ -3,35 +3,35 @@ a = document.getElementById('gb').childNodes[0].childNodes[0].childNodes[0];
 var element = document.createElement('div');
 element.setAttribute('class','gb_e gb_f');
 element.setAttribute('id','appendedByChromeExtention1');
-element.innerHTML = '<a class="gb_b" href="http://javblog.org/category/jav/" target="_blank">javblog</a>';
+element.innerHTML = '<a class="gb_g" href="http://javblog.org/category/jav/" target="_blank">javblog</a>';
 a.appendChild(element);
 
 var element = document.createElement('div');
 element.setAttribute('class','gb_e gb_f');
 element.setAttribute('id','appendedByChromeExtention2');
-element.innerHTML = '<a class="gb_b" href="http://latestjav.net/" target="_blank">latestjav</a>';
+element.innerHTML = '<a class="gb_g" href="http://latestjav.net/" target="_blank">latestjav</a>';
 a.appendChild(element);
 
 var element = document.createElement('div');
 element.setAttribute('class','gb_e gb_f');
 element.setAttribute('id','appendedByChromeExtention3');
-element.innerHTML = '<a class="gb_b" href="http://www.intporn.com/forums/asian-jav-movies" target="_blank">intporn</a>';
+element.innerHTML = '<a class="gb_g" href="http://www.intporn.com/forums/asian-jav-movies" target="_blank">intporn</a>';
 a.appendChild(element);
 
 var element = document.createElement('div');
 element.setAttribute('class','gb_e gb_f');
 element.setAttribute('id','appendedByChromeExtention4');
-element.innerHTML = '<a class="gb_b" href="http://tyakuero-dvd-iv.info/" target="_blank">tyakuero</a>';
+element.innerHTML = '<a class="gb_g" href="http://tyakuero-dvd-iv.info/" target="_blank">tyakuero</a>';
 a.appendChild(element);
 
 var element = document.createElement('div');
 element.setAttribute('class','gb_e gb_f');
 element.setAttribute('id','appendedByChromeExtention5');
-element.innerHTML = '<a class="gb_b" href="http://pant.su" target="_blank">pantsu</a>';
+element.innerHTML = '<a class="gb_g" href="http://pant.su" target="_blank">pantsu</a>';
 a.appendChild(element);
 
 var element = document.createElement('div');
 element.setAttribute('class','gb_e gb_f');
 element.setAttribute('id','appendedByChromeExtention6');
-element.innerHTML = '<a class="gb_b" href="http://www.aventertainments.com" target="_blank">AVE</a>';
+element.innerHTML = '<a class="gb_g" href="http://www.aventertainments.com" target="_blank">AVE</a>';
 a.appendChild(element);
